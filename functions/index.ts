ca2afb7c4b4d933e677d5349a80b692d2e1f37e0
Aakash N S <aakashns@jovian.com>
2024-01-12 05:32:03 +0530

    Simplify bio

--- functions/index.ts
+++ functions/index.ts
@@ -4,13 +4,7 @@ import { renderMarkdown } from "@/src/ui/markdown";
 import { renderPageHeading } from "@/src/ui/pageHeading";
 import { renderPostItem } from "@/src/ui/postItem";
 
-const bio = `
-I'm the founder and CEO of [Jovian](https://jovian.com), a learning platform for 
-software development and data science. I've previously worked as a software 
-engineer at [Twitter](https://twitter.com) and I hold a bachelor's degree in
-computer science from [IIT Bombay](https://www.cse.iitb.ac.in). Online courses 
-I've created have garnered over 10 million views on 
-[YouTube](https://youtube.com/@jovianhq).`;
+const bio = `I write, code, and teach.`;
 
 const homePage = renderLayout(
   /* HTML */ `
