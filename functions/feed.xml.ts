5e9b0d087f04947876fdabe72afb9c8fc9bc05b2
Aakash N S <1560745+aakashns@users.noreply.github.com>
2024-02-14 09:44:32 +0530

    fix item and channel tags

--- functions/feed.xml.ts
+++ functions/feed.xml.ts
@@ -8,6 +8,7 @@ export function onRequestGet(context: EventContext<unknown, string, unknown>) {
     <item>
       <title>${post.title}</title>
       <link>${baseUrl}/${post.slug}</link>
+      <guid>${baseUrl}/${post.slug}</guid>
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
       <description><![CDATA[${post.content}]]></description>
     </item>
@@ -16,11 +17,12 @@ export function onRequestGet(context: EventContext<unknown, string, unknown>) {
     .join("\n");
 
   const rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0">
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
         <title>Aakash N S</title>
         <link>${baseUrl}</link>
         <description>I write, code, and teach</description>
+        <atom:link href="${baseUrl}/feed.xml" rel="self" type="application/rss+xml" />
         ${items}
       </channel>
     </rss>
@@ -28,7 +30,7 @@ export function onRequestGet(context: EventContext<unknown, string, unknown>) {
 
   return new Response(rssFeed, {
     headers: {
-      "content-type": "text/xml;charset=UTF-8",
+      "content-type": "application/rss+xml;charset=UTF-8",
       "Cache-Control": "stale-while-revalidate=60",
     },
   });
