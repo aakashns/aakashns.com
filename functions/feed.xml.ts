3362d49b4d3e19a420842d20011a9cad493be05e
Aakash N S <1560745+aakashns@users.noreply.github.com>
2024-02-14 13:16:26 +0530

    Switch to Atom feed

--- functions/feed.xml.ts
+++ functions/feed.xml.ts
@@ -1,37 +1,39 @@
-import { makeMetaDescription } from "shared/lib";
-import { posts } from "shared/posts";
+import { Post, posts } from "shared/posts";
 import { renderMarkdown } from "shared/ui/markdown";
-import * as he from "he";
 
 export function onRequestGet(context: EventContext<unknown, string, unknown>) {
   const baseUrl = new URL(context.request.url).origin;
-  const items = posts
-    .map(
-      (post) => `
-    <item>
-      <title>${post.title}</title>
-      <link>${baseUrl}/${post.slug}</link>
-      <guid>${baseUrl}/${post.slug}</guid>
-      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-      <content type="html">${he.encode(renderMarkdown(post.content))}</content>
-    </item>
-  `
-    )
-    .join("\n");
 
-  const rssFeed = /*xml*/ `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-      <channel>
-        <title>Aakash N S</title>
-        <link>${baseUrl}</link>
-        <description>I write, code, and teach</description>
-        <atom:link href="${baseUrl}/feed.xml" rel="self" type="application/rss+xml" />
-        ${items}
-      </channel>
-    </rss>
+  const entryTemplate = (post: Post) => /*xml*/ `
+    <entry>
+      <title>${post.title}</title>
+      <link href="${baseUrl}/${post.slug}" />
+      <id>${baseUrl}/${post.slug}</id>
+      <updated>${new Date(post.date).toISOString()}</updated>
+      <content type="html">
+        <![CDATA[${renderMarkdown(post.content)}]]>
+      </content>
+    </entry>
   `;
 
-  return new Response(rssFeed, {
+  const entries = posts.map(entryTemplate).join("\n");
+
+  const feed = /*xml*/ `<?xml version="1.0" encoding="utf-8"?>
+    <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>Aakash N S</title>
+    <subtitle>I write, code, and teach.</subtitle>
+    <link href="${baseUrl}/feed.xml" rel="self"/>
+    <link href="${baseUrl}" />
+    <updated>${new Date(posts[0].date).toISOString()}</updated>
+    <id>${baseUrl}</id>
+    <author>
+      <name>Aakash N S</name>
+    </author>
+      ${entries}
+    </feed>
+`;
+
+  return new Response(feed, {
     headers: {
       "content-type": "text/xml;charset=UTF-8",
       "Cache-Control": "stale-while-revalidate=60",
